a2f6c1937da131beeac642fa818a4c6fb018fc71
vitor <vitor.m2005@aluno.ifsc.edu.br>
2026-06-25 20:49:03 -0300

    teste2

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: './database.sqlite',
-    logging: false // Deixa o terminal limpo
+    logging: false
 });
 
 module.exports = sequelize;
